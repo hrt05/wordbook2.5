1f666d09b6a44334e831947e9f81e7017830fdff
hrt05 <hirate05program@gmail.com>
2025-12-04 23:36:00 +0900

    console.logで完了を表示できることを確認

--- src/lib/firebase/auth/login.ts
+++ src/lib/firebase/auth/login.ts
@@ -5,6 +5,7 @@ import { FirebaseError } from "firebase/app";
 const login = async (email: string, password: string) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
+    console.log("ログイン完了", signInWithEmailAndPassword);
   } catch (e) {
     if (e instanceof FirebaseError) console.log(e);
   }
