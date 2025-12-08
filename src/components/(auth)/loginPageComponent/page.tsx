43f3809c4257712feaa8d070a13ba87d115f7956
hrt05 <hirate05program@gmail.com>
2025-12-09 01:48:25 +0900

    アラートと、ページ遷移を完了させました。

--- src/components/(auth)/loginPageComponent/page.tsx
+++ src/components/(auth)/loginPageComponent/page.tsx
@@ -4,13 +4,17 @@ import { Button, TextField } from "@charcoal-ui/react";
 import styles from "./loginComponent.module.css";
 import { useState } from "react";
 import login from "@/lib/firebase/auth/login";
+import { useRouter } from "next/navigation";
 
 const LoginPageComponent = () => {
+  const router = useRouter()
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = async () => {
-    await login(email, password);
+    const ok = await login(email, password);
+    if(ok) router.push("/")
   };
 
   return (
